599f2c66035b246772dc4781ffe4671e3982aa32
Noah Pope <nlpope@tutanota.com>
2020-04-22 17:15:26 -0700

    initial commit

---
+++ src/containers/Course/Course.js
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+
+class Course extends Component {
+  state = {
+    courseTitle: "",
+  };
+
+  //   query params
+  componentDidMount() {
+    this.setTitle();
+  }
+
+  //source of infinite loop
+  //on mount it updates, then it checks for update, then it checks f...
+  componentDidUpdate() {
+    this.setTitle();
+  }
+
+  setTitle() {
+    console.log(this.props);
+    const query = new URLSearchParams(this.props.location.search);
+    for (let param of query.entries()) {
+      if (this.state.courseTitle !== param[1]) {
+        this.setState({ courseTitle: param[1] });
+      }
+      //   console.log(param);
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        {console.log(this.props.match)}
+        <h1>{this.state.courseTitle}</h1>
+        <p>
+          You selected the Course with ID: {this.props.match.params.courseId}
+        </p>
+      </div>
+    );
+  }
+}
+
+export default Course;
